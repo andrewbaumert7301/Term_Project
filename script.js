899f863127d7bea1cb07c0d6db60fd63015cadc3
Andrew Baumert <andrewbaumert7301@gmail.com>
2025-11-19 20:03:46 -0500

    Did more clean up. I think all thats left is the "Content Sections" for each of the 3 webpages, including the php database

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Image file paths to rotate through
 const images = [
-    "img/food1.jpg",
-    "img/food2.jpg",
-    "img/food3.jpg",
-    "img/food4.jpg"
+    "img/food1.jpg", //https://www.pexels.com/photo/burger-with-fried-fries-on-black-plate-with-sauce-on-the-side-1199957/
+    "img/food2.jpg", //https://www.pexels.com/photo/fries-and-burger-on-plate-70497/
+    "img/food3.jpg", //https://www.pexels.com/photo/hamburger-and-fries-photo-2983101/
+    "img/food4.jpg" //https://www.pexels.com/photo/close-up-photo-of-burger-1639562/
 ];
 
 let index = 0;        // Tracks which image is currently shown
